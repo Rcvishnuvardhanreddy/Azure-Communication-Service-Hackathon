e2dcd6f920836f5fc60bdfb17fa47e9b5925f160
Vishnu Racha <ChencuVishnuVardhanReddy.Racha@insightsoftware.com>
2024-03-20 09:25:08 -0700

    adding demo application to git repo

---
+++ ACS Email Demo/Demo/Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Azure;
+using Azure.Communication.Email;
+
+// This code retrieves your connection string from an environment variable.
+string connectionString = "endpoint=https://notificationservicedemo.unitedstates.communication.azure.com/;accesskey=ihJYTq2ED4+kCSImDJOF5sBA81UIb4MrCRzIhg+RIphKR+5ayQ8f/m7vVR+frT3iE8Gw6t8VZjT7TxLQnGIZYw==";
+var emailClient = new EmailClient(connectionString);
+
+
+EmailSendOperation emailSendOperation = emailClient.Send(
+    WaitUntil.Completed,
+    senderAddress: "DoNotReply@f48f6b1c-4be0-4419-a3a8-02e2002efc81.azurecomm.net",
+    recipientAddress: "rcvishnuvardhan@gmail.com",
+    subject: "Test Email",
+    htmlContent: "<html><h1>Hello world via email.</h1l></html>",
+    plainTextContent: "Hello world via email.");
+
+var result = emailSendOperation;
---
+++ ACS Email Demo/Demo/SendSMS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Mail;
+    using Azure;
+    using Azure.Communication;
+    using Azure.Communication.Sms;
+
+    namespace SendSMS
+    {
+        internal class Program
+        {
+            private static void Main()
+            {
+                var connectionString = "endpoint=https://notificationservicedemo.unitedstates.communication.azure.com/;accesskey=ihJYTq2ED4+kCSImDJOF5sBA81UIb4MrCRzIhg+RIphKR+5ayQ8f/m7vVR+frT3iE8Gw6t8VZjT7TxLQnGIZYw==\""; // Find your Communication Services resource in the Azure portal
+                SmsClient smsClient = new SmsClient(connectionString);
+
+                SmsSendResult sendResult = smsClient.Send(
+                    from: "<from-phone-number>", // Your E.164 formatted from phone number used to send SMS
+                    to: "<to-phone-number>", // E.164 formatted recipient phone number
+                    message: "Hello 👋🏻");
+                Console.WriteLine($"Message id {sendResult.MessageId}");
+
+                Response<IReadOnlyList<SmsSendResult>> response = smsClient.Send(
+                from: "<from-phone-number>",
+                to: new string[] { "<to-phone-number-1>", "<to-phone-number-2>" }, // E.164 formatted recipient phone numbers
+                message: "Hello 👋🏻",
+                options: new SmsSendOptions(enableDeliveryReport: true) // OPTIONAL
+                {
+                    Tag = "greeting", // custom tags
+                });
+
+                IEnumerable<SmsSendResult> results = response.Value;
+                foreach (SmsSendResult result in results)
+                {
+                    Console.WriteLine($"Sms id: {result.MessageId}");
+                    Console.WriteLine($"Send Result Successful: {result.Successful}");
+                }
+            }
+        }
+    }
+
+}
